4021479b8bef9420cef9fd0a0f47e1cc546ead11
Sean Corbett <scorbett@bu.edu>
2017-03-20 21:18:02 -0400

    Updating parallel interface

--- R/celda.R
+++ R/celda.R
@@ -1,20 +1,17 @@
+#' @import foreach
 #' @export
 celda = function(counts, model, sample.label=NULL, nchains=1, cores=1, ...) {
-  suppressPackageStartupMessages(require(foreach))
-  suppressPackageStartupMessages(require(doParallel))
+  cl = parallel::makeCluster(cores)
+  doParallel::registerDoParallel(cl)
   
-  cl<-makeCluster(cores)
-  registerDoParallel(cl)
-  
-  runs =expand.grid(chain=1:nchains, ...)
+  runs = expand.grid(chain=1:nchains, ...)
   print(runs)  
+  
   res.list = foreach(i = 1:nrow(runs), .export=model, .combine = c, .multicombine=TRUE) %dopar% {
-    source("celda_CG.R")  ## quick hack to bring in auxillary functions, will need to eventually hanlde via ".export", not sure about how to handle in a package
-    source("celda_functions.R")
     res = do.call(model, c(list(counts=counts, sample.label=sample.label), c(runs[i,-1])))
     all = list(runs[i,], res)
     return(list(res))
   }  
   
-  return(res.list)
+  return(list(run.params=runs, res.list=res.list))
 }
