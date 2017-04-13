3ea33b713010f86b99637b103c0db1b6360f4db6
Sean Corbett <scorbett@bu.edu>
2017-04-13 18:35:49 -0400

    Use random seeds for each chain. Fixes #11

--- R/celda.R
+++ R/celda.R
@@ -7,8 +7,14 @@ celda = function(counts, model, sample.label=NULL, nchains=1, cores=1, ...) {
   runs = expand.grid(chain=1:nchains, ...)
   print(runs)  
   
+  
+  # Generate unique random seeds for each chain. 
+  # Reusable between parameter combinations (e.g. unique combinations of K and L)
+  seeds = sample(1:1000000, size=nchains)
+  
   res.list = foreach(i = 1:nrow(runs), .export=model, .combine = c, .multicombine=TRUE) %dopar% {
-    res = do.call(model, c(list(counts=counts, sample.label=sample.label), c(runs[i,-1])))
+    seed = seeds[ifelse(i %% nchains == 0, nchains, i %% nchains)]
+    res = do.call(model, c(list(counts=counts, sample.label=sample.label, seed=seed), c(runs[i,-1])))
     all = list(runs[i,], res)
     return(list(res))
   }  
