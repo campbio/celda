b5f3aa62c6862f442c369ae3f3f6300098c980df
Sean Corbett <scorbett@bu.edu>
2017-05-05 09:58:31 -0400

    Adding man page for toy dataset.

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' A toy count matrix for use with celda. 
+#b' A toy count matrix for use with celda. 
 #'
 #' @format A matrix of simulated gene counts with 10 rows (genes) and 10 columns (cells)
 #' @source \url{http://github.com/definitelysean/celda}
