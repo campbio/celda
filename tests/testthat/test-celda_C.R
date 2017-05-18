ba6db8d3e2c5742b6c01b1309363d3d3587fdda4
ykoga07 <007ky.0@gmail.com>
2017-05-18 17:57:58 -0400

    20170518
    Test commit

--- tests/testthat/test-celda_C.R
+++ tests/testthat/test-celda_C.R
@@ -1,1 +1,12 @@
 # celda_C.R
+library(celda)
+context("Testing celda_C")
+
+celdac <- simulateCells.celda_C(K=10)
+celdaC.res <- celda(counts=celdac$counts,model="celda_C",nchains=1,cores=4,K=10)
+
+test_that("finalClusterAssignment.celda_C",{
+  expect_equal(celdaC.res$res.list[[1]]$z, finalClusterAssignment(celdaC.res$res.list[[1]]))
+})
+
+
