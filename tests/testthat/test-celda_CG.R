ce5d554377d72db0110f3777d9646f734df66165
Yusuke Koga <ykoga07@bu.edu>
2017-06-20 16:23:55 -0400

    Add unit test for visualize_model_performance
    Former-commit-id: fbf18d16b93aae2344d3b1525a9f129a96143c36

--- tests/testthat/test-celda_CG.R
+++ tests/testthat/test-celda_CG.R
@@ -1,1 +1,11 @@
 # celda_CG.R
+library(celda)
+context("Testing celda_CG")
+
+celdacg <- simulateCells.celda_CG(K=10,L=10)
+celdaCG.res <- celda(counts=celdacg$counts,model="celda_CG",nchains=1,cores=4,K=10,L=10)
+
+test_that("CheckingVisualizeModelPerformace",{
+        expect_equal(TRUE, all(!is.na(visualize_model_performance(celdacg))))
+        })
+
--- tests/testthat/test-celda_G.R
+++ tests/testthat/test-celda_G.R
@@ -1,1 +1,12 @@
-# celda_C.R
+#celda_G
+library(celda)
+context("Testing celda_G")
+
+celdag <- simulateCells.celda_G(L=10)
+
+celdaG.res <- celda(counts=celdag$counts,model="celda_G",nchains=1,cores=4,L=10)
+
+test_that("CheckingVisualizeModelPerformace",{
+        expect_equal(TRUE, all(!is.na(visualize_model_performance(celdaG.res))))
+        })
+
