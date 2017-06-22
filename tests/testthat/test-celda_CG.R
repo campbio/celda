6cd679164f67cc3a50fd9b40cf5bf9ea6af60486
Yusuke Koga <ykoga07@bu.edu>
2017-06-22 16:15:27 -0400

    change test files
    Former-commit-id: da7127fc7f00aaa359d2ea5a29e3ed4225d56e1f

--- tests/testthat/test-celda_CG.R
+++ tests/testthat/test-celda_CG.R
@@ -3,7 +3,7 @@ library(celda)
 context("Testing celda_CG")
 
 celdacg <- simulateCells.celda_CG(K=10,L=10)
-celdaCG.res <- celda(counts=celdacg$counts,model="celda_CG",nchains=1,cores=4,K=10,L=10)
+celdaCG.res <- celda(counts=celdacg$counts, model="celda_CG", nchains=1, K=10, L=10)
 
 test_that("CheckingVisualizeModelPerformace",{
         expect_equal(TRUE, all(!is.na(visualize_model_performance(celdaCG.res))))
