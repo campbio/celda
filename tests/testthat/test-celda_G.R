da7127fc7f00aaa359d2ea5a29e3ed4225d56e1f
Yusuke Koga <ykoga07@bu.edu>
2017-06-22 16:15:27 -0400

    change test files

--- tests/testthat/test-celda_G.R
+++ tests/testthat/test-celda_G.R
@@ -4,7 +4,7 @@ context("Testing celda_G")
 
 celdag <- simulateCells.celda_G(L=10)
 
-celdaG.res <- celda(counts=celdag$counts,model="celda_G",nchains=1,cores=4,L=10)
+celdaG.res <- celda(counts=celdag$counts, model="celda_G", nchains=1, L=10)
 
 test_that("CheckingVisualizeModelPerformace",{
         expect_equal(TRUE, all(!is.na(visualize_model_performance(celdaG.res))))
