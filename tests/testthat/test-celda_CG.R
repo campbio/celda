5316a4adc107ddebadb9431fef12f7947ffcfd94
Yusuke Koga <ykoga07@bu.edu>
2017-06-26 16:52:23 -0400

    Fix error in test-celda_CG.R
    Former-commit-id: c6c3c00fbca101185c82ad129af25d17ac248be5

--- tests/testthat/test-celda_CG.R
+++ tests/testthat/test-celda_CG.R
@@ -2,8 +2,8 @@
 library(celda)
 context("Testing celda_CG")
 
-celdacg <- simulateCells.celda_CG(K = 3, L = 5, G = 200)
-celdaCG.res <- celda(counts=celdacg$counts, nchains=1, K = 2:4, L = 4:6, ncores=1)
+celdacg <- simulateCells.celda_CG(K = 3, L = 5)
+celdaCG.res <- celda(counts=celdacg$counts, nchains=1, K = 2:4, L = 4:6, ncores=1, model = "celda_CG")
 
 test_that("CheckingVisualizeModelPerformace",{
         expect_equal(TRUE, all(!is.na(visualize_model_performance(celdaCG.res))))
