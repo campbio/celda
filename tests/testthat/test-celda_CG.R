45192ad0556cdba058b91d12bb4196b0df929d4b
Yusuke Koga <ykoga07@bu.edu>
2017-06-26 14:14:46 -0400

    Change test-celda_CG.R, make K=2:4/L=4:6
    Former-commit-id: 7b986945bf5d1c2d09e836f9a900db699cdcd82a

--- tests/testthat/test-celda_CG.R
+++ tests/testthat/test-celda_CG.R
@@ -3,7 +3,7 @@ library(celda)
 context("Testing celda_CG")
 
 celdacg <- simulateCells.celda_CG(K = 3, L = 5, G = 200)
-celdaCG.res <- celda_CG(counts=celdacg$counts, nchains=1, K = 3, L = 5, ncores=1)
+celdaCG.res <- celda(counts=celdacg$counts, nchains=1, K = 2:4, L = 4:6, ncores=1)
 
 test_that("CheckingVisualizeModelPerformace",{
         expect_equal(TRUE, all(!is.na(visualize_model_performance(celdaCG.res))))
