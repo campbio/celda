5821db2dfa9954e6e87af7d65486afc475584ed4
Yusuke Koga <ykoga07@bu.edu>
2017-06-26 13:50:03 -0400

    Changed celda_CG.R test, checking with multiple K/L

--- tests/testthat/test-celda_CG.R
+++ tests/testthat/test-celda_CG.R
@@ -2,8 +2,8 @@
 library(celda)
 context("Testing celda_CG")
 
-celdacg <- simulateCells.celda_CG(K=10,L=10)
-celdaCG.res <- celda(counts=celdacg$counts, model="celda_CG", nchains=1, K=10, L=10)
+celdacg <- simulateCells.celda_CG(K = 3, L = 5, G = 200)
+celdaCG.res <- celda_CG(counts=celdacg$counts, nchains=1, K = 3, L = 5, ncores=1)
 
 test_that("CheckingVisualizeModelPerformace",{
         expect_equal(TRUE, all(!is.na(visualize_model_performance(celdaCG.res))))
