fd3f73df6dd71c43e87635cfd2d156c164953f1d
Sean Corbett <scorbett@bu.edu>
2017-10-25 12:14:50 -0400

    Documenting ... parameters in loglik calculation fns. #137

--- R/celda_CG.R
+++ R/celda_CG.R
@@ -42,6 +42,7 @@
 #' @param beta The Dirichlet distribution parameter for Phi; adds a pseudocount to each transcriptional state within each cell.
 #' @param delta The Dirichlet distribution parameter for Eta; adds a gene pseudocount to the numbers of genes each state.
 #' @param gamma The Dirichlet distribution parameter for Psi; adds a pseudocount to each gene within each transcriptional state.
+#' @param ... Additional parameters 
 calculateLoglikFromVariables.celda_CG = function(counts, s, z, y, K, L, alpha, beta, delta, gamma, ...) {
   
   ## Calculate for "Theta" component
