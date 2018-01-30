32de7bc686dfecc224e264d78ad41c1fe92e723b
Sean Corbett <scorbett@bu.edu>
2018-01-30 14:57:30 -0500

    Refactored visualizeModelPerformance and tests

--- tests/testthat/test-celda_CG.R
+++ tests/testthat/test-celda_CG.R
@@ -2,10 +2,11 @@
 library(celda)
 context("Testing celda_CG")
 
-celdacg <- simulateCells.celda_CG(K = 3, L = 5)
-celdaCG.res <- celda(counts=celdacg$counts, nchains=1, K = 2:4, L = 4:6, ncores=1, model = "celda_CG")
+celdacg <- simulateCells.celda_CG(K=3, L=5)
+celdaCG.res <- celda(counts=celdacg$counts, nchains=1, K = 2:4, L = 4:6, 
+                     ncores=1, model="celda_CG")
 
-test_that("CheckingVisualizeModelPerformace",{
-        expect_equal(TRUE, all(!is.na(visualizeModelPerformance(celdaCG.res))))
-        })
+test_that("visualizeModelPerformance Returns a Plot",{
+  expect_equal(TRUE, all(!is.na(visualizeModelPerformance(celdaCG.res, celdacg$counts))))
+})
 
