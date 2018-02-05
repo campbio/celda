dc4c4b5e04a9103f0fad6482afd29b5e77f4f5ee
Sean Corbett <scorbett@bu.edu>
2018-02-05 00:50:25 -0500

    Calculate perplexity w/ resample in celda_C. #187
    Former-commit-id: 856d1cddf04d3d34c5e4be9d649f58034429fb41

--- R/celda_C.R
+++ R/celda_C.R
@@ -417,40 +417,58 @@ celdaHeatmap.celda_C = function(celda.mod, counts, ...) {
 
 
 #' @export
-calculatePerplexity.celda_C = function(celda.mod, counts, precision=128) {
+calculatePerplexity.celda_C = function(celda.mod, counts, 
+                                       resample=1, precision=128) {
   if (!compareCountMatrix(counts, celda.mod$count.checksum)) {
     stop("Provided count matrix was not used to generate the provided celda model.")
   }
   
-  factorized = factorizeMatrix(celda.mod, counts, "posterior")
-  theta = log(factorized$posterior$sample.states)
-  phi   = log(factorized$posterior$gene.states)
-  sl = celda.mod$sample.label
-  
-  inner.log.prob = (t(phi) %*% counts) + theta[, sl]  
-  inner.log.prob = Rmpfr::mpfr(inner.log.prob, precision)
-  inner.log.prob.exp = exp(inner.log.prob)
-  
-  log.px = 0
-  for(i in 1:ncol(inner.log.prob.exp)) {
-    log.px = log.px + Rmpfr::asNumeric(log(sum(inner.log.prob.exp[, i])))
+  # By default, just calculate perplexity for the original
+  # counts matrix; otherwise, 
+  countsList = list(counts)
+  for(i in 2:resample) {
+    countsList[[i]] = resampleCountMatrix(counts)
+    i = i - 1  
   }
   
-  perplexity = exp(-(log.px/sum(counts)))
-  return(perplexity)
+  # TODO Can try to turn into a single giant matrix multiplication by duplicating
+  #     phi / theta / sl
+  # TODO Cast to sparse matrices?
+  perplexities = lapply(countsList, function(counts){ 
+    factorized = factorizeMatrix(celda.mod, counts, "posterior")
+    theta = log(factorized$posterior$sample.states)
+    phi = log(factorized$posterior$gene.states)
+    sl = celda.mod$sample.label
+    
+    inner.log.prob = (t(phi) %*% counts) + theta[, sl]  
+    inner.log.prob = Rmpfr::mpfr(inner.log.prob, precision)
+    inner.log.prob.exp = exp(inner.log.prob)
+    
+    log.px = 0
+    for(i in 1:ncol(inner.log.prob.exp)) {
+      log.px = log.px + Rmpfr::asNumeric(log(sum(inner.log.prob.exp[, i])))
+    }
+    
+    perplexity = exp(-(log.px/sum(counts)))
+    return(perplexity)
+  })
+  
+  return(perplexities)
 }  
 
 
-#' visualizeModelPerformance for celda Cell clustering function
+#' visualizeModelPerformance for celda cell clustering function
 #' @param celda.list A celda_list object returned from celda()
 #' @param counts The counts used to generate the celda.list results
 #' @param method One of "perplexity", "loglik"
+#' @param resamples Number of resamplings to evaluate for perplexity, if method = "perplexity"
 #' @param title Title for the plot
 #' @param log Currently not working for celda_C objects
 #' @import Rmpfr
 #' @export
 visualizeModelPerformance.celda_C = function(celda.list, counts,
                                              method="perplexity", 
+                                             resample=NA,
                                              title="Model Performance (All Chains)",
                                              log = F) {
   
