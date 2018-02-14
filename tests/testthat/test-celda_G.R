1a42145997afc39966541e369f8646d876e24344
Yusuke Koga <ykoga07@bu.edu>
2018-02-14 15:09:22 -0500

    Add more descriptions to tests
    Former-commit-id: 8d2c3c62b2e005829ba6b44c50c4d0458cb02091

--- tests/testthat/test-celda_G.R
+++ tests/testthat/test-celda_G.R
@@ -2,17 +2,11 @@
 library(celda)
 context("Testing celda_G")
 
-#celdaG 
-# celdaG.sim <- simulateCells(L = 5, model = "celda_G")
-# save(celdaG.sim,file = "celdaGsim.rda")
-# celdaG.res <- celda(counts = celdaG.sim$counts, model = "celda_G", nchains = 2, L = 5)
-# save(celdaG.res, file = "celdaG.rda")
-
-
 load("../celdaGsim.rda")
 load("../celdaG.rda")
 model_G = getModel(celdaG.res, L = 5, best = "loglik")
 factorized <- factorizeMatrix(celda.mod = model_G, counts = celdaG.sim$counts)
+counts.matrix <- celdaG.sim$counts
 
 #Making sure getModel if functioning correctly
 test_that(desc = "Sanity checking getModel",{
@@ -26,61 +20,24 @@ test_that(desc = "Checking factorize matrix, counts vs proportions",{
 })
 
 #Checking dimension of factorize matrix
-test_that(desc = "Checking factorize matrix",{
+test_that(desc = "Checking factorize matrix dimension size",{
   expect_equal(5, ncol(factorized$proportions$gene.states))  
 })
 
-
-#01-31-18 
-#Creating unit tests for celda_functions
-counts.matrix <- celdaG.sim$counts
-
-# #cosineDist
-# test_that(desc = "Checking cosineDist",{
-#   expect_equal("dist",class(cosineDist(factorized$posterior$population.states)))
-# })
-# 
-# #cosine
-# test_that(desc = "Checking cosineDist",{
-#   expect_equal("matrix",class(cosine(t(factorized$posterior$population.states))))
-# })
-# 
-# #spearmanDist
-# test_that(desc = "Checking spearmanDist",{
-#   expect_equal("dist",class(spearmanDist(factorized$posterior$population.states)))
-# })
-
-#normalizeLogProbs
-
-
 #normalizeCounts
-test_that(desc = "Checking normalizeCounts",{
+test_that(desc = "Making sure normalizeCounts doesn't change dimensions of counts matrix",{
   norm.counts <- normalizeCounts(counts.matrix)
   expect_equal(dim(norm.counts),dim(counts.matrix))
   expect_equal(rownames(norm.counts),rownames(counts.matrix))
   expect_equal(colnames(norm.counts),colnames(counts.matrix))
 })
 
-# #reorder.label.by.size
-# test_that(desc = "Checking reorder.label.by.size",{
-#   expect_true(all(table((reorder.label.by.size(model$z,model$K))$new.labels)[1] >= 
-#                     table((reorder.label.by.size(model$z,model$K))$new.labels)))
-# })
-# 
-# #reorder.labels.by.size.then.counts
-# test_that(desc = "Checking reorder.labels.by.size.then.counts",{
-#   expect_true(all(table((reorder.labels.by.size.then.counts(counts = celdaG$counts,z = model$z,y = model$y, L = model$K, L = model$L))$new.labels)[1] >= 
-#                     table((reorder.labels.by.size.then.counts(counts = celdaG$counts,z = model$z,y = model$y, L = model$K, L = model$L))$new.labels)))
-# })
-
 #recodeClusterY
-test_that(desc = "Checking recodeClusterY",{
+test_that(desc = "Checking recodeClusterY gives/doesn't give error",{
   expect_error(recodeClusterY(celda.mod = model_G, from = NULL, to = ))
-})
-
-#recodeClusterZ
-test_that(desc = "Checking recodeClusterZ",{
-  expect_error(recodeClusterY(celda.mod = model_G, from = NULL, to = ))
+  expect_error(recodeClusterY(celda.mod = model_G, from=c(1,2,3,4,5), to = c(1,2,3,4,6)))
+  new.recoded = recodeClusterY(celda.mod = model_G, from=c(1,2,3,4,5), to = c(5,4,3,2,1))
+  expect_equal(model_G$y == 1, new.recoded$y == 5)
 })
 
 #compareCountMatrix
@@ -93,21 +50,16 @@ test_that(desc = "Checking distinct_colors",{
   expect_equal(distinct_colors(2), c("#FF9999","#99FFFF"))
 })
 
-#initialize.cluster
-#test_that(desc = "Checking distinct_colors",{
-#  expect_equal(initialize.cluster(N = 3, len = 3), c(2,3,1))
-#})
-
 
 ###renderCeldaHeatmap###
-test_that(desc = "Checking renderCeldaHeatmap",{
+test_that(desc = "Checking renderCeldaHeatmap output",{
   expect_equal(names(renderCeldaHeatmap(counts = celdaG.sim$counts, z = model_G$z, y = model_G$y)),
                c("tree_row","tree_col","kmeans","gtable"))
 })
 
 ##feature_selection.R##
 #topRank
-test_that(desc = "Checking topRank",{
+test_that(desc = "Checking topRank function",{
   expect_equal(names(topRank(fm = factorized$proportions$gene.states)),
                c("index","names"))
 })
@@ -115,7 +67,7 @@ test_that(desc = "Checking topRank",{
 
 
 ##celda_G.R##
-test_that(desc = "Checking celda_G",{
+test_that(desc = "Making sure celda_G runs without errors",{
   celdaG.res <- celda(counts = celdaG.sim$counts, model = "celda_G", nchains = 2, L = 5)
   expect_equal(celdaG.res$run.params$chain,c(1,2))
 })
