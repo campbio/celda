a136676b499765bf1b8062cc6f3abd7fdc6c726f
Yusuke Koga <ykoga07@bu.edu>
2018-03-06 09:22:46 -0500

    Modify vignette, add pre-created model to speed up vignette creation, add tsne fxn
    Former-commit-id: 24bf24b2432ef0b320a0745ea6394d2629c9d224

--- inst/doc/celda-analysis.R
+++ inst/doc/celda-analysis.R
@@ -18,7 +18,7 @@ library(Rtsne)
 library(reshape2)
 
 ## ------------------------------------------------------------------------
-sim_counts = simulateCells("celda_CG", K = 3, L = 10)
+sim_counts = simulateCells("celda_CG", K = 6, L = 8, S = 10)
 
 ## ------------------------------------------------------------------------
 dim(sim_counts$counts)
@@ -35,9 +35,10 @@ table(sim_counts$sample.label)
 ## ------------------------------------------------------------------------
 table(sim_counts$z, sim_counts$sample.label)
 
-## ---- fig.show='hold', warning = FALSE, message = FALSE------------------
+## ---- warning = FALSE, message = FALSE-----------------------------------
 
-celda.res = celda(counts = sim_counts$counts, model = "celda_CG", K = 3, L = 10, max.iter = 10, cores = 1, nchains = 1)
+celda.res = celda(counts = sim_counts$counts, model = "celda_CG", K = 6, L = 9, max.iter = 10, cores = 1, nchains = 1)
+
 
 ## ------------------------------------------------------------------------
 z = celda.res$res.list[[1]]$z
@@ -48,12 +49,12 @@ table(y, sim_counts$y)
 
 
 ## ---- fig.width = 7, fig.height = 7, warning = FALSE, message = FALSE----
-norm.counts <- normalizeCounts(sim_counts$counts)
+norm.counts = normalizeCounts(sim_counts$counts, scale.factor = 1e6)
 renderCeldaHeatmap(counts = norm.counts, z=z, y=y, normalize = NULL, color_scheme = "divergent",cluster_gene = TRUE, cluster_cell = TRUE)
 
 ## ------------------------------------------------------------------------
-model <- getModel(celda.res, K = 3, L = 10, best = "loglik")
-factorized <- factorizeMatrix(model, sim_counts$count)
+model = getModel(celda.res, K = 6, L = 9, best = "loglik")
+factorized = factorizeMatrix(model, sim_counts$count)
 
 ## ------------------------------------------------------------------------
 dim(factorized$proportions$gene.states)
@@ -61,7 +62,7 @@ head(factorized$proportions$gene.states)
 
 ## ------------------------------------------------------------------------
 dim(factorized$proportions$cell.states)
-factorized$proportions$cell.states[1:10,1:3]
+factorized$proportions$cell.states[1:6,1:9]
 
 ## ------------------------------------------------------------------------
 pop.states = factorized$proportions$population.states
@@ -69,7 +70,7 @@ dim(pop.states)
 head(pop.states)
 
 ## ---- fig.width = 7, fig.height = 7--------------------------------------
-data.pca <- prcomp(t(scale(t(factorized$proportions$cell.states))),scale = F, center = F)
+data.pca = prcomp(t(scale(t(factorized$proportions$cell.states))),scale = F, center = F)
 
 plotDrCluster(dim1 = data.pca$rotation[,1], dim2 = data.pca$rotation[,2], cluster = celda.res$res.list[[1]]$z)
 
@@ -82,11 +83,11 @@ renderCeldaHeatmap(pop.states, color_scheme = "sequential", show_cellnames=T, sh
 rel.states = sweep(pop.states, 1, rowSums(pop.states), "/")
 renderCeldaHeatmap(rel.states, color_scheme = "sequential", show_cellnames=T, show_genenames=T, breaks = NA, z= 1:ncol(rel.states), y = 1:nrow(rel.states))
 
-## ------------------------------------------------------------------------
-celda.res.list <- celda(counts = sim_counts$counts,K = 2:4, L = 9:11, nchains = 3, max.iter = 10, model = "celda_CG", cores = 1)
+## ---- eval = FALSE-------------------------------------------------------
+#  celda.res.list = celda(counts = sim_counts$counts,K = 2:4, L = 9:11, nchains = 3, max.iter = 10, model = "celda_CG", cores = 1)
 
-## ------------------------------------------------------------------------
-model = getModel(celda.res.list, K = 3, L = 10, best = "loglik")
+## ---- eval = FALSE-------------------------------------------------------
+#  model = getModel(celda.res.list, K = 3, L = 10, best = "loglik")
 
 ## ----setup, include=FALSE------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE,fig.align = "center")
@@ -102,39 +103,42 @@ head(rownames(pbmc_data))
 head(colnames(pbmc_data))
 
 ## ------------------------------------------------------------------------
-pbmc_select <- pbmc_data[rowSums(pbmc_data>4) > 4,]
+pbmc_select = pbmc_data[rowSums(pbmc_data>4) > 4,]
 
-## ---- message = FALSE, verbose = FALSE-----------------------------------
-pbmc.res = celda(pbmc_select, K = 10, L = 20, cores = 1, model = "celda_CG", nchains = 8, max.iter = 100)
-model<-getModel(pbmc.res, K = 10, L = 20, best = "loglik")
+## ---- eval = FALSE-------------------------------------------------------
+#  pbmc_res = celda(pbmc_select, K = 10, L = 20, cores = 1, model = "celda_CG", nchains = 8, max.iter = 100)
+
+## ------------------------------------------------------------------------
+model = getModel(pbmc_res, K = 10, L = 15, best = "loglik")
 
 ## ------------------------------------------------------------------------
 factorize.matrix = factorizeMatrix(model, counts=pbmc_select)
-top.genes <- topRank(factorize.matrix$proportions$gene.states, n = 25)
+top.genes = topRank(factorize.matrix$proportions$gene.states, n = 25)
 
 ## ------------------------------------------------------------------------
 top.genes$names$L3
 
 ## ------------------------------------------------------------------------
-top.genes$names$L4
+top.genes$names$L9
 
 ## ---- fig.width = 7, fig.height = 7--------------------------------------
-top.genes.ix <- unique(unlist(top.genes$index))
-norm.pbmc.counts <- normalizeCounts(pbmc_select)
+top.genes.ix = unique(unlist(top.genes$index))
+norm.pbmc.counts = normalizeCounts(pbmc_select)
 renderCeldaHeatmap(norm.pbmc.counts[top.genes.ix,], z = model$z, y = model$y[top.genes.ix], normalize = NULL, color_scheme = "divergent")
 
 ## ---- message = FALSE----------------------------------------------------
-norm_pbmc <- normalizeCounts(factorize.matrix$counts$cell.states)
+norm_pbmc = normalizeCounts(factorize.matrix$counts$cell.states)
 set.seed(123)
-tsne <- Rtsne(t(norm_pbmc), pca = FALSE, max_iter = 2000)
-pbmc_tsne <- tsne$Y
+pbmc_tsne = celdaTsne(counts = pbmc_select, celda.mod = model)
 
 ## ---- fig.width = 7, fig.height = 7--------------------------------------
 plotDrCluster(dim1 = pbmc_tsne[,1], dim2 = pbmc_tsne[,2], cluster = as.factor(model$z))
 
 plotDrState(dim1 = pbmc_tsne[,1], dim2 = pbmc_tsne[,2], matrix = factorize.matrix$proportions$cell.states, rescale = TRUE)
 
-marker.genes <- c("ENSG00000168685_IL7R","ENSG00000198851_CD3E","ENSG00000105374_NKG7","ENSG00000203747_FCGR3A","ENSG00000090382_LYZ","ENSG00000179639_FCER1A","ENSG00000156738_MS4A1", "ENSG00000163736_PPBP")
-gene.counts <- pbmc_select[marker.genes,]
+marker.genes = c("ENSG00000168685_IL7R","ENSG00000198851_CD3E","ENSG00000105374_NKG7",
+                 "ENSG00000203747_FCGR3A","ENSG00000090382_LYZ","ENSG00000179639_FCER1A",
+                 "ENSG00000156738_MS4A1", "ENSG00000163736_PPBP")
+gene.counts = pbmc_select[marker.genes,]
 plotDrGene(dim1 = pbmc_tsne[,1],dim2 = pbmc_tsne[,2], matrix = gene.counts, rescale = TRUE)
 
