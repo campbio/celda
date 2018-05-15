fac24b4f4e428142311a651f1e8847ffb01e30d2
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-05-15 11:09:34 -0400

    Refactored GibbsProbY so that elements of the n.C.by.TS were not being repeatedly reprocessed with each iteration of the inner for loop. This resulted in a dramatic speed improvement
    Former-commit-id: 5ac0dfb7a216406b18ae726b9fbafb6ff493eef7

--- R/celda_G.R
+++ R/celda_G.R
@@ -167,7 +167,7 @@ cG.calcGibbsProbY = function(counts.t, n.C.by.TS, n.by.TS, nG.by.TS, n.by.G, y, 
   probs = matrix(NA, ncol=nG, nrow=L)
   temp.nG.by.TS = nG.by.TS 
   temp.n.by.TS = n.by.TS 
-  temp.n.C.by.TS = n.C.by.TS
+#  temp.n.C.by.TS = n.C.by.TS
 
   ix <- sample(1:nG)
   for(i in ix) {
@@ -175,26 +175,37 @@ cG.calcGibbsProbY = function(counts.t, n.C.by.TS, n.by.TS, nG.by.TS, n.by.G, y, 
 	## Subtract current gene counts from matrices
 	nG.by.TS[y[i]] = nG.by.TS[y[i]] - 1L
 	n.by.TS[y[i]] = n.by.TS[y[i]] - n.by.G[i]
-	n.C.by.TS[,y[i]] = n.C.by.TS[,y[i]] - counts.t[,i]
-
+	
+	n.C.by.TS_1 = n.C.by.TS
+	n.C.by.TS_1[,y[i]] = n.C.by.TS_1[,y[i]] - counts.t[,i]
+    n.C.by.TS_1 = .colSums(lgamma(n.C.by.TS_1 + beta), nrow(n.C.by.TS), ncol(n.C.by.TS))
+    
+	n.C.by.TS_2 = n.C.by.TS
+	other.ix = (1:L)[-y[i]]
+	n.C.by.TS_2[,other.ix] = n.C.by.TS_2[,other.ix] + counts.t[,i]
+    n.C.by.TS_2 = .colSums(lgamma(n.C.by.TS_2 + beta), nrow(n.C.by.TS), ncol(n.C.by.TS))
+    	
 	## Calculate probabilities for each state
 	for(j in 1:L) {
 	
 	  temp.nG.by.TS = nG.by.TS 
 	  temp.n.by.TS = n.by.TS 
-	  temp.n.C.by.TS = n.C.by.TS
+#	  temp.n.C.by.TS = n.C.by.TS
 	
 	  temp.nG.by.TS[j] = temp.nG.by.TS[j] + 1L      
 	  temp.n.by.TS[j] = temp.n.by.TS[j] + n.by.G[i]
-	  temp.n.C.by.TS[,j] = temp.n.C.by.TS[,j] + counts.t[,i]
+#	  temp.n.C.by.TS[,j] = temp.n.C.by.TS[,j] + counts.t[,i]
+
 
       pseudo.nG.by.TS = temp.nG.by.TS
 	  pseudo.nG.by.TS[temp.nG.by.TS == 0L] = 1L
 	  pseudo.nG = sum(pseudo.nG.by.TS)
 
+	  other.ix = (1:L)[-j]
       probs[j,i] <- 	sum(lgamma(pseudo.nG.by.TS + gamma)) -
 						sum(lgamma(sum(pseudo.nG.by.TS + gamma))) +
-						sum(lgamma(temp.n.C.by.TS + beta)) +
+						sum(n.C.by.TS_1[other.ix]) + n.C.by.TS_2[j] +
+#						sum(lgamma(temp.n.C.by.TS + beta)) +
 						sum(lgamma(pseudo.nG.by.TS * delta)) -
 						(pseudo.nG * lgamma(delta)) -
 						sum(lgamma(temp.n.by.TS + (pseudo.nG.by.TS * delta)))
@@ -202,11 +213,17 @@ cG.calcGibbsProbY = function(counts.t, n.C.by.TS, n.by.TS, nG.by.TS, n.by.G, y, 
     }
 
 	## Sample next state and add back counts
+	prev.y = y[i]
 	if(isTRUE(do.sample)) y[i] = sample.ll(probs[,i])
+	
+	if(prev.y != y[i]) {
+	  n.C.by.TS[,prev.y] = n.C.by.TS[,prev.y] - counts.t[,i]
+	  n.C.by.TS[,y[i]] = n.C.by.TS[,y[i]] + counts.t[,i]
+	}
 	
 	nG.by.TS[y[i]] = nG.by.TS[y[i]] + 1L
 	n.by.TS[y[i]] = n.by.TS[y[i]] + n.by.G[i]
-	n.C.by.TS[,y[i]] = n.C.by.TS[,y[i]] + counts.t[,i]
+	#n.C.by.TS[,y[i]] = n.C.by.TS[,y[i]] + counts.t[,i]
   }
   
   return(list(n.C.by.TS=n.C.by.TS, nG.by.TS=nG.by.TS, n.by.TS=n.by.TS, y=y, probs=probs))
