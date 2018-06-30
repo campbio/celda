87f729e41b92add4f6d6e3a6ee6b3c784096d897
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-06-30 16:32:50 -0400

    Needed to added statement to load in previously simulated results
    Former-commit-id: 6416f5d4080366fe7930b9b6ce1ff6a5a962456c

--- tests/testthat/test-celda_C.R
+++ tests/testthat/test-celda_C.R
@@ -24,6 +24,7 @@ test_that(desc = "Checking diffExpBetweenCellStates",{
 })
 
 ##celda_C.R##
+load("../celdaCsim.rda")
 test_that(desc = "Checking celda_C to see if it runs without errors",{
   celdaC.res <- celda(counts = celdaC.sim$counts, model = "celda_C",  nchains = 2, K = c(5,10), max.iter = 15)
   expect_true(class(celdaC.res) == "celda_list")  # Only best chain is returned
