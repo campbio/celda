80089d0459b5e3ec96e3db991a0217a830882a66
Sean Corbett <scorbett@bu.edu>
2018-07-01 22:30:57 -0400

    Updating documentation. Explicitly import SummarizedExperiment where needed.
    Former-commit-id: 346071a799846a58a450d52b2e5e5056c71d0fb7

--- R/diffExp.R
+++ R/diffExp.R
@@ -16,7 +16,7 @@
 #'    than this threshold (1 by default).
 #' @return Data frame containing a ranked list (based on the absolute value of log2fc) of putative markers,
 #'    and associated statistics (p-value, log2fc and FDR).
-#' @import data.table
+#' @import data.table SummarizedExperiment
 #' @export
 diffExpBetweenCellStates <- function(counts, celda.mod, c1, c2 = NULL, only.pos = FALSE, log2fc.threshold = NULL, fdr.threshold = 1) {
   if (is.null(counts)) {
