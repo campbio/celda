2cdc161947f3c6c0e4456a180f7417f7d87a8e69
Sean <definitelysean@users.noreply.github.com>
2018-07-24 18:38:00 -0400

    Typecast Counts in factorizeMatrix() Subfunctions (#284)
    * Typecast Count Matrices Before Matrix Factorization  (#283)
    * Run counts through processCounts() before factorizing matrices. Fixes #279

--- R/split_clusters.R
+++ R/split_clusters.R
@@ -183,8 +183,35 @@ cCG.splitZ = function(counts, m.CP.by.S, n.TS.by.C, n.TS.by.CP, n.by.G, n.by.TS,
 
 
 
-cCG.splitY = function(counts, y, m.CP.by.S, n.G.by.CP, n.TS.by.C, n.TS.by.CP, n.by.G, n.by.TS, nG.by.TS, n.CP, s, z, K, L, nS, nG, alpha, beta, delta, gamma, y.prob, max.clusters.to.try=10, min.cell=3) {
+cCG.splitY = function(counts, y, m.CP.by.S, n.G.by.CP, n.TS.by.C, n.TS.by.CP, n.by.G, n.by.TS, nG.by.TS, n.CP, s, z, K, L, nS, nG, alpha, beta, delta, gamma, y.prob, max.clusters.to.try=10, K.subclusters=10, min.cell=3) {
 
+  #########################
+  ## First, the cell dimension of the original matrix will be reduced by splitting each z cluster into 'K.subclusters'.
+  #########################
+  
+  ## This will not be as big as the original matrix (which can take a lot of time to process with large number of cells), but not as small as the 'n.G.by.CP' with current z assignments
+  z.ta = tabulate(z, K)
+  z.non.empty = which(z.ta > 0)
+  temp.z = rep(0, length(z))
+  current.top.z = 0
+  for(i in z.non.empty) { 
+    ix = z == i
+    if(z.ta[i] <= K.subclusters) {
+      temp.z[ix] = (current.top.z + 1):(current.top.z + z.ta[i])
+    } else {
+      clustLabel = suppressMessages(celda_C(counts[,z == i], K=K.subclusters, max.iter=5, split.on.iter=-1, split.on.last=FALSE))
+      temp.z[ix] = clustLabel$z + current.top.z 
+    }
+    current.top.z = max(temp.z, na.rm=TRUE)
+  }
+  
+  ## Decompose counts according to new/temp z labels
+  temp.n.G.by.CP = colSumByGroup(counts, group=temp.z, K=current.top.z)
+
+  #########################
+  ## Second, different y splits will be estimated and tested
+  #########################
+  
   ## Identify clusters to split
   y.ta = tabulate(y, L)
   y.to.split = which(y.ta >= min.cell)
@@ -194,11 +221,11 @@ cCG.splitY = function(counts, y, m.CP.by.S, n.G.by.CP, n.TS.by.C, n.TS.by.CP, n.
     m = paste0(date(), " ... Cluster sizes too small. No additional splitting was performed.") 
     return(list(y=y, m.CP.by.S=m.CP.by.S, n.TS.by.CP=n.TS.by.CP, n.CP=n.CP, message=m))  
   }
-  
+
   ## Loop through each split-able Z and perform split
   clust.split = vector("list", L)
   for(i in y.to.split) { 
-    clustLabel = suppressMessages(celda_G(counts[y == i,], L=2, max.iter=5, split.on.iter=-1, split.on.last=FALSE))
+    clustLabel = suppressMessages(celda_G(temp.n.G.by.CP[y == i,], L=2, max.iter=5, split.on.iter=-1, split.on.last=FALSE))
     clust.split[[i]] = clustLabel$y
   }
 
@@ -229,7 +256,6 @@ cCG.splitY = function(counts, y, m.CP.by.S, n.G.by.CP, n.TS.by.C, n.TS.by.CP, n.
     previous.y = new.y
   } 
   y.to.shuffle = head(order(ll.shuffle, decreasing = TRUE, na.last=NA), n = max.clusters.to.try)
-
   
   pairs = c(NA, NA)
   split.ix = 2
