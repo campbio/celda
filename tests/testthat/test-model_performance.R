fe2dfa9ae46f2266b267f21a57b9577aaf6003a5
Sean Corbett <scorbett@bu.edu>
2018-07-26 13:22:09 -0400

    Toggle count validation in calculatePerplexity.
    Former-commit-id: 2493789f20b9e85da6bbca3e86db7fcbd6255baa

--- tests/testthat/test-model_performance.R
+++ tests/testthat/test-model_performance.R
@@ -8,14 +8,14 @@ load("../celdaCGsim.rda")
 load("../celdaCG.rda")
 
 test_that(desc = "Testing calculatePerplexityWithResampling", {
-  perplexity.results = calculatePerplexityWithResampling(celdaCG.res, celdaCG.sim$counts, resample=2)
+  perplexity.results = calculatePerplexityWithResampling(celdaCG.res, celdaCG.sim$counts, resample=2, validate.counts=FALSE)
   expect_named(perplexity.results, c("perplexity.info", "plot"))
   expect_is(perplexity.results$plot, "ggplot")
 })
 
 
 test_that(desc = "Testing visualizePerplexityByKL", {
-  perplexity.results = calculatePerplexityWithResampling(celdaCG.res, celdaCG.sim$counts, resample=2)
+  perplexity.results = calculatePerplexityWithResampling(celdaCG.res, celdaCG.sim$counts, resample=2, validate.counts=FALSE)
   plot.obj = visualizePerplexity(perplexity.results$perplexity.info)
   expect_is(plot.obj, "ggplot")
 })
