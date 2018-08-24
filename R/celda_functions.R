36635b8d3c9d9c2c8dcd86be56ad90a6ee03e64d
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-08-24 12:00:49 -0400

    Made s3 function for celdaProbabilityMap. Updated documentation. Updated test units

--- R/celda_functions.R
+++ R/celda_functions.R
@@ -78,57 +78,7 @@ normalizeCounts = function(counts, normalize=c("proportion", "cpm", "median", "m
   return(norm)
 }
   
-  
-reorder.label.by.size = function(z, K) {
-  z.ta = as.numeric(names(sort(table(factor(z, levels=1:K)), decreasing=TRUE)))
-  
-  new.z = z
-  for(i in 1:length(z.ta)) {
-    new.z[z == z.ta[i]] = i
-  }
-  return(list(new.labels=new.z, map=z.ta))
-}  
-
-reorder.labels.by.size.then.counts = function(counts, z, y, K, L) {
-  z.ta = as.numeric(names(sort(table(factor(z, levels=1:K)), decreasing=TRUE)))
-  
-  new.z = rep(NA, length(z))
-  for(i in 1:length(z.ta)) {
-    new.z[z == z.ta[i]] = i
-  }
-
-  n.TS.by.C = rowsum(counts, group=y, reorder=TRUE)
-  n.CP.by.TS = rowsum(t(n.TS.by.C), group=new.z, reorder=TRUE)
-  TS.order = lapply(1:K, function(i) order(n.CP.by.TS[i,], decreasing=TRUE))  
-  
-  ## Determine the number of transcriptional states to order by in each cell population
-  if(K > L) {
-    num.L.per.K = rep(1:0, c(K, K-L))
-  } else { 
-    #num.L.per.K = rep(c(ceiling(L/K), floor(L/K)), c(ceiling(K/2),floor(K/2)))
-    temp = rep(floor(L/K), K)
-    num.L.per.K = temp + rep(1:0, c(L-sum(temp), K-L+sum(temp)))
-  }
-
-  ## Cycle through each K and assign the state(s) with the highest counts
-  y.to.choose.from = 1:L  
-  cp.with.ts = which(num.L.per.K > 0)  
-  y.order = c()
-  for(i in cp.with.ts) {
-    ix = setdiff(TS.order[[i]], y.order)[1:num.L.per.K[i]]
-    y.order = c(y.order, ix)
-    y.to.choose.from = setdiff(y.to.choose.from, ix)
-  }
-  
-  new.y = rep(NA, length(y))
-  for(i in 1:length(y.order)) {
-    new.y[y == y.order[i]] = i
-  }
-
-  return(list(z=new.z, y=new.y, z.map=z.ta, y.map=y.order))  
-}  
-
-
+ 
 #' Obtain the gene module of a gene of interest
 #' 
 #' This function will output the gene module of a specific gene(s) from a celda model
