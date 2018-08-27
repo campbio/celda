7a241ac2a4560bbbc5e8e60840cc46763014e70d
Yusuke Koga <ykoga07@bu.edu>
2018-08-27 11:20:18 -0400

    Tidying up order of tests
    Former-commit-id: 482d901e86955d6dce1fed9f860a9888197e822b

--- tests/testthat/test-celda_C.R
+++ tests/testthat/test-celda_C.R
@@ -6,71 +6,82 @@ load("../celdaCsim.rda")
 load("../celdaC.rda")
 counts.matrix <- celdaC.sim$counts
 model_C = filterCeldaList(celdaC.res, K = 5)[[1]]
-factorized = factorizeMatrix(counts=counts.matrix, celda.mod=model_C)
+factorized = factorizeMatrix(counts=counts.matrix, celda.mod = model_C)
 
-##distinct_colors##
-test_that(desc = "Checking distinct_colors",{
+#Checking pre-loaded data
+#Making sure filterCeldaList if functioning correctly
+test_that(desc = "Sanity checking filterCeldaList", {
+  expect_equal(celdaC.res$content.type, class(model_C))
+})
+
+#distinct_colors#
+test_that(desc = "Checking distinct_colors", {
   expect_equal(distinct_colors(2), c("#FF4D4D", "#4DFFFF"))
 })
 
-###Convenience functions###
-
-test_that(desc = "Checking finalClusterAssignment, celdaC",{
+#Convenience functions#
+test_that(desc = "Checking finalClusterAssignment, celdaC", {
   expect_true(all(finalClusterAssignment(celda.mod = model_C) <= 5))
 })
 
-test_that(desc = "Checking clusterProbability, celdaC",{
+test_that(desc = "Checking clusterProbability, celdaC", {
   expect_true(all(rowSums(clusterProbability(model_C, counts = counts.matrix)[[1]]) == 1))
 })
 
 
-test_that(desc = "Checking getK",{
+test_that(desc = "Checking getK", {
   expect_equal(5,getK(celda.mod = model_C))
 })
 
-###celdaHeatmap###
-test_that(desc = "Checking renderCeldaHeatmap to see if it runs without errors",{
-  expect_equal(names(celdaHeatmap(celda.mod = model_C, counts = celdaC.sim$counts)),
-               c("tree_row","tree_col","kmeans","gtable"))
+test_that(desc = "simulateCells.celda_C returns correctly typed output", {
+  sim.res = simulateCells(model = "celda_C")
+  expect_equal(typeof(sim.res$counts), "integer")
 })
 
-##feature_selection.R##
+#celda_C.R#
+#celdaGridSearch
+test_that(desc = "Checking celda_C to see if it runs without errors", {
+  celdaC.res <- celdaGridSearch(counts = celdaC.sim$counts, celda.mod = "celda_C",  nchains = 2, K.to.test = c(5,10), max.iter = 15)
+  expect_true(class(celdaC.res)[1] == "celda_list")  # Only best chain is returned
+})
+
+#Ensure calculateLoglikFromVariables calculates the expected values
+test_that(desc = "calculateLoglikFromVariables.celda_C returns correct output for various params", {
+  expect_lt(calculateLoglikFromVariables.celda_C(counts = celdaC.sim$counts,z = celdaC.sim$z,
+                                                 K = celdaC.sim$K, alpha = 1, beta = 1,sample.label = celdaC.sim$sample.label), 0)
+})
+
+#normalizeCounts
+test_that(desc = "Making sure normalizeCounts doesn't change dimensions of counts matrix", {
+  norm.counts <- normalizeCounts(counts.matrix)
+  expect_equal(dim(norm.counts),dim(counts.matrix))
+  expect_equal(rownames(norm.counts),rownames(counts.matrix))
+  expect_equal(colnames(norm.counts),colnames(counts.matrix))
+})
+
+#feature_selection.R#
 #topRank
-test_that(desc = "Checking topRank to see if it runs without errors",{
+test_that(desc = "Checking topRank to see if it runs without errors", {
   top.rank <- topRank(matrix = factorized$proportions$gene.states, n = 1000)
   expect_equal(names(top.rank),
                c("index","names"))
 })
 
-#differentialExpression
-test_that(desc = "Checking differentialExpression",{
+#celdaHeatmap#
+test_that(desc = "Checking renderCeldaHeatmap to see if it runs without errors", {
+  expect_equal(names(celdaHeatmap(celda.mod = model_C, counts = celdaC.sim$counts)),
+               c("tree_row", "tree_col", "kmeans", "gtable"))
+})
+
+#differentialExpression#
+test_that(desc = "Checking differentialExpression", {
   diffexp_K1 <- differentialExpression(counts = counts.matrix, 
                                          celda.mod = model_C, c1 = 1)
-  expect_equal(class(diffexp_K1), c("data.table","data.frame"))
+  expect_equal(class(diffexp_K1), c("data.table", "data.frame"))
 })
 
 
-test_that(desc = "simulateCells.celda_C returns correctly typed output", {
-  sim.res = simulateCells(model="celda_C")
-  expect_equal(typeof(sim.res$counts), "integer")
-})
-
-# Ensure calculateLoglikFromVariables calculates the expected values
-test_that(desc = "calculateLoglikFromVariables.celda_C returns correct output for various params", {
-  expect_lt(calculateLoglikFromVariables.celda_C(counts = celdaC.sim$counts,z = celdaC.sim$z,
-                                                 K = celdaC.sim$K, alpha = 1, beta = 1,sample.label = celdaC.sim$sample.label 
-  ),
-  0)
-})
-
-##celda_C.R##
-test_that(desc = "Checking celda_C to see if it runs without errors",{
-  celdaC.res <- celdaGridSearch(counts = celdaC.sim$counts, celda.mod = "celda_C",  nchains = 2, K.to.test = c(5,10), max.iter = 15)
-  expect_true(class(celdaC.res)[1] == "celda_list")  # Only best chain is returned
-})
-
-
-##celdaTsne##
+#celdaTsne#
 test_that(desc = "testing celdaTsne", {
   celdaC.tsne <-celdaTsne(counts = celdaC.sim$counts,celda.mod = model_C, max.iter = 50, max.cells = 500)
   plotDrCluster(dim1 = celdaC.tsne[,1], dim2 = celdaC.tsne[,2], cluster = model_C$z)
