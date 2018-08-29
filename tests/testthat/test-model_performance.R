d90564a793021da086f81c6eb2e44b67be303aaa
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-08-29 14:57:28 -0400

    Added test units for celda_G perplexity and model performance
    Former-commit-id: 1832290ad38f729e95ae883e47d27e415ef13e8f

--- tests/testthat/test-model_performance.R
+++ tests/testthat/test-model_performance.R
@@ -13,6 +13,8 @@ load("../celdaG.rda")
 
 test_that(desc = "Testing calculatePerplexityWithResampling for celda_CG", {
   expect_equal(is.null(celdaCG.res$perplexity), TRUE)
+  expect_error(plotGridSearchPerplexity(celdaCG.res))
+
   celdaCG.res = calculatePerplexityWithResampling(celdaCG.sim$counts, celdaCG.res, resample=2)
   expect_equal(is.null(celdaCG.res$perplexity), FALSE)
   expect_is(celdaCG.res, "celda_list")
@@ -20,11 +22,20 @@ test_that(desc = "Testing calculatePerplexityWithResampling for celda_CG", {
 
   plot.obj = plotGridSearchPerplexity(celdaCG.res)
   expect_is(plot.obj, "ggplot")
+
+  model_CG = filterCeldaList(celdaCG.res, K = 5, L = 3)[[1]]
+  expect_error(celdaCG.res <- calculatePerplexityWithResampling(celdaCG.sim$counts, model_CG, resample=2))
+  expect_error(celdaCG.res <- calculatePerplexityWithResampling(celdaCG.sim$counts, celdaCG.res, resample='a'))
+
+  celdaC.res = calculatePerplexityWithResampling(celdaC.sim$counts, celdaC.res, resample=2)
+  expect_error(plotGridSearchPerplexity.celda_CG(celdaC.res))
 })
 
 
 test_that(desc = "Testing calculatePerplexityWithResampling for celda_C", {
   expect_equal(is.null(celdaC.res$perplexity), TRUE)
+  expect_error(plotGridSearchPerplexity(celdaC.res))
+
   celdaC.res = calculatePerplexityWithResampling(celdaC.sim$counts, celdaC.res, resample=2)
   expect_equal(is.null(celdaC.res$perplexity), FALSE)
   expect_is(celdaC.res, "celda_list")
@@ -32,11 +43,16 @@ test_that(desc = "Testing calculatePerplexityWithResampling for celda_C", {
 
   plot.obj = plotGridSearchPerplexity(celdaC.res)
   expect_is(plot.obj, "ggplot")
+
+  celdaG.res = calculatePerplexityWithResampling(celdaG.sim$counts, celdaG.res, resample=2)
+  expect_error(plotGridSearchPerplexity.celda_C(celdaG.res))
 })
 
 
 test_that(desc = "Testing calculatePerplexityWithResampling for celda_G", {
   expect_equal(is.null(celdaG.res$perplexity), TRUE)
+  expect_error(plotGridSearchPerplexity(celdaG.res))
+
   celdaG.res = calculatePerplexityWithResampling(celdaG.sim$counts, celdaG.res, resample=2)
   expect_equal(is.null(celdaG.res$perplexity), FALSE)
   expect_is(celdaG.res, "celda_list")
@@ -44,6 +60,15 @@ test_that(desc = "Testing calculatePerplexityWithResampling for celda_G", {
 
   plot.obj = plotGridSearchPerplexity(celdaG.res)
   expect_is(plot.obj, "ggplot")
+
+  celdaC.res = calculatePerplexityWithResampling(celdaC.sim$counts, celdaC.res, resample=2)
+  expect_error(plotGridSearchPerplexity.celda_G(celdaC.res))
+
+  model_G <- filterCeldaList(celdaG.res, L = 5)[[1]]
+  res <- calculatePerplexity.celda_G(celdaG.sim$counts, model_G)
+  res2 <- calculatePerplexity.celda_G(celdaG.sim$counts, model_G, new.counts = celdaG.sim$counts[,-1] + 1)
+  
+  expect_error(res <- calculatePerplexity.celda_G(celdaG.sim$counts[-1,], model_G))
 })
 
 
