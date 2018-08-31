da9b8d19ae8a98ec5888b28b2671ac79f8eeb818
Sean Corbett <scorbett@bu.edu>
2018-08-31 17:43:42 -0400

    Docs: calculateLoglikFromVariables examples
    Former-commit-id: 6839068a732b3211a5693dedf26e4cdef433c463

--- R/s3_generics.R
+++ R/s3_generics.R
@@ -73,10 +73,19 @@ celdaHeatmap <- function(counts, celda.mod, ...) {
 #' 
 #' @param counts The counts matrix used to generate the provided cluster assignments.
 #' @param celda.mod Celda model. Options available in `celda::available.models`.
-#' @return The log-likelihood of the provided cluster assignment for the provided counts matrix.
 #' @param ... Additional parameters.
+#' @return The log-likelihood of the provided cluster assignment for the provided counts matrix.
+#' @examples
+#' celda.sim = simulateCells(model="celda_CG")
+#' celda.mod = "celda_CG"
+#' loglik = calculateLoglikFromVariables(celda.sim$counts, celda.mod, 
+#'                                       sample.label=celda.sim$sample.label,
+#'                                       z=celda.sim$z, y=celda.sim$y,
+#'                                       K=celda.sim$K, L=celda.sim$L,
+#'                                       alpha=celda.sim$alpha, beta=celda.sim$beta,
+#'                                       gamma=celda.sim$gamma, delta=celda.sim$delta)
 #' @export
-calculateLoglikFromVariables <- function(counts, celda.mod, ...) {
+calculateLoglikFromVariables = function(counts, celda.mod, ...) {
   class(counts) = c(celda.mod)
   do.call(paste("calculateLoglikFromVariables.", celda.mod, sep=""),
           list(counts, ...))
