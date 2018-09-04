f17387ff8d6e38a490e85189596f32ca523679af
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-09-03 21:31:55 -0400

    Refactored celdaGridSearch, filterCeldaList, and selectBestModel to use an argument called 'params' to pass parameters rather than explicitly listing and validating each one
    Former-commit-id: 988c0384bdf10345d50fb1ede78bc09de684fc01

--- R/celdaGridSearch.R
+++ R/celdaGridSearch.R
@@ -16,13 +16,13 @@ available_models = c("celda_C", "celda_G", "celda_CG")
 #' @param beta Numeric. Concentration parameter for Phi. Adds a pseudocount to each feature module in each cell population. Default 1. 
 #' @param delta Numeric. Concentration parameter for Psi. Adds a pseudocount to each feature in each module. Default 1. 
 #' @param gamma Numeric. Concentration parameter for Eta. Adds a pseudocount to the number of features in each module. Default 1. 
-#' @param max.iter Integer. Maximum number of iterations of Gibbs sampling to perform. Default 20. 
+#' @param max.iter Integer. Maximum number of iterations of Gibbs sampling to perform. Default 200. 
 #' @param z.init Integer vector. Sets initial starting values of z. If NULL, starting values for each cell will be randomly sampled from 1:K. Default NULL.
 #' @param y.init Integer vector. Sets initial starting values of y. If NULL, starting values for each feature will be randomly sampled from 1:L. Default NULL.
 #' @param stop.iter Integer. Number of iterations without improvement in the log likelihood to stop inference. Default 20.
 #' @param split.on.iter Integer. On every `split.on.iter` iteration, a heuristic will be applied to determine if a cell population or feature module should be reassigned and another cell population or feature module should be split into two clusters. To disable splitting, set to -1. Default 20.
 #' @param nchains Integer. Number of random cluster initializations. Default 1. 
-#' @param bestChainsOnly Logical. Whether to return only the best chain (by final log-likelihood) per K/L combination. Default TRUE. 
+#' @param best.only Logical. Whether to return only the chain with the highest log likelihood per combination of parameters. Default TRUE. 
 #' @param cores Integer. The number of cores to use for parallel Gibbs sampling. Default 1.
 #' @param seed Integer. Passed to set.seed(). Default 12345.  
 #' @param verbose Logical. Whether to print log messages during celda chain execution. Default TRUE. 
@@ -35,16 +35,33 @@ available_models = c("celda_C", "celda_G", "celda_CG")
 #'                              K.to.test=2:4, L.to.test=9:11, max.iter=2, nchains=1)
 #' @import foreach
 #' @export
-celdaGridSearch = function(counts, model, sample.label=NULL, K.to.test=NULL, L.to.test=NULL, alpha=1, beta=1, 
-                 delta=1, gamma=1, max.iter=20, z.init=NULL, y.init=NULL,
-                 stop.iter=10, split.on.iter=10, nchains=1, 
-                 bestChainsOnly=TRUE, cores=1, seed=12345, verbose=TRUE, 
-                 logfile.prefix="Celda") {
+celdaGridSearch = function(counts, model, params,
+				    max.iter=200, cores=1, best.only=TRUE,
+				    seed=12345, verbose=TRUE, nchains=3,
+                    logfile.prefix="Celda") {
  
-  validateArgs(counts, model, sample.label, nchains, cores, seed, K.to.test=K.to.test, L=L.to.test)
-  params.list = buildParamList(counts, model, sample.label, alpha, beta, delta,
-                               gamma, max.iter, z.init, y.init, stop.iter, split.on.iter,
-                               nchains, cores, seed)
+  ## Check parameters
+  model.params = as.list(formals(model))
+  if(!all(names(params) %in% names(model.params))) {
+    bad.params = setdiff(names(params), names(model.params))
+    stop(paste0("The following elements in 'params' are not arguments of '", model, "': ", paste(bad.params, collapse=","), "."))
+  }
+  
+  model.params.required = setdiff(names(model.params[model.params == ""]), "counts")
+  if(!all(model.params.required %in% names(params))) {
+    missing.params = setdiff(model.params.required, names(params))
+    stop(paste0("The following arguments are not in 'params' but are required for '", model, "': ", paste(missing.params, collapse=","), "."))
+  }
+  if(any(c("z.init", "y.init") %in% names(params))) {
+    stop("Setting 'z.init' and 'y.init' in 'params' is not currently supported.")
+  }
+  
+  # Set up parameter combinations for each individual chain
+  run.params = expand.grid(c(chain=list(1:nchains), params))
+  run.params = cbind(index = 1:nrow(run.params), run.params)
+
+  # Pre-generate a set of random seeds to be used for each chain
+  all.seeds = seed:(seed + nchains - 1)
 
   logMessages("--------------------------------------------------------------------", logfile=NULL, append=FALSE, verbose=verbose)  
   logMessages("Starting celdaGridSearch with", model, logfile=NULL, append=TRUE, verbose=verbose)
@@ -52,53 +69,46 @@ celdaGridSearch = function(counts, model, sample.label=NULL, K.to.test=NULL, L.t
   logMessages("--------------------------------------------------------------------", logfile=NULL, append=TRUE, verbose=verbose)  
   start.time = Sys.time()
 
-
-  cl = parallel::makeCluster(cores)
-  doParallel::registerDoParallel(cl)
-  
-  # Details for each model parameter / chain combination 
-  run.params = expand.grid(plyr::compact(list(chain=1:nchains, K=K.to.test, L=L.to.test)))
-  run.params$index = as.numeric(rownames(run.params))
-  
-  # Pre-generate a set of random seeds to be used for each chain
-  all.seeds = seed:(seed + nchains - 1)
-  
   # An MD5 checksum of the count matrix. Passed to models so
   # later on, we can check on celda_* model objects which
   # count matrix was used.
   counts = processCounts(counts)
   count.checksum = digest::digest(counts, algo="md5")
-  params.list$count.checksum = count.checksum
-  params.list$nchains = 1
-   
+
+  ## Use DoParallel to loop through each combination of parameters
+  cl = parallel::makeCluster(cores)
+  doParallel::registerDoParallel(cl)   
   res.list = foreach(i = 1:nrow(run.params), .export=model, .combine = c, .multicombine=TRUE) %dopar% {
-    chain.params = append(params.list,
-                          as.list(dplyr::select(run.params[i,],
-                                                dplyr::matches("K|L"))))
-    chain.params$seed = all.seeds[ifelse(i %% nchains == 0, nchains, i %% nchains)]
     
-    ## Generate a unique log file name based on given prefix and parameters
+    ## Set up chain parameter list
+    current.run = c(run.params[i,])
+    chain.params = list()
+    for(j in names(params)) {
+      chain.params[[j]] = current.run[[j]]
+    }
+    chain.params$counts = counts
+    chain.params$seed = all.seeds[ifelse(i %% nchains == 0, nchains, i %% nchains)]
+    chain.params$max.iter = max.iter
+    chain.params$nchain = 1
+    chain.params$count.checksum = count.checksum
     chain.params$verbose = verbose
     chain.params$logfile = paste0(logfile.prefix, "_", paste(paste(colnames(run.params), run.params[i,], sep="-"), collapse="_"),  "_Seed-", chain.params$seed, "_log.txt")                                
+    
+    ## Run model
     res = do.call(model, chain.params)
     return(list(res))
   }
-  parallel::stopCluster(cl)
-  celda.res = list(run.params=run.params, res.list=res.list, 
+  parallel::stopCluster(cl)  
+  
+  logliks = sapply(res.list, function(mod) { mod[["finalLogLik"]] })
+  run.params = cbind(run.params, log_likelihood=logliks)
+    
+  celda.res = list(run.params=run.params, params=params, res.list=res.list, 
                    content.type=model, count.checksum=count.checksum)
   class(celda.res) = c("celda_list", model)
   
-  if (isTRUE(bestChainsOnly)) {
-    new.run.params = unique(dplyr::select(run.params, -index, -chain))
-    new.run.params$index = 1:nrow(new.run.params)
-    best.chains = apply(new.run.params, 1,
-                        function(params) {
-                          k = if ("K" %in% names(params)) params[["K"]] else NULL
-                          l = if ("L" %in% names(params)) params[["L"]] else NULL
-                          selectBestModel(celda.res, k, l)
-                        })
-    celda.res$run.params = new.run.params
-    celda.res$res.list = best.chains
+  if (isTRUE(best.only)) {
+    celda.res = selectBestModel(celda.res) 
   }
   
   end.time = Sys.time()
