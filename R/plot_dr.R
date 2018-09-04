19bef469a2c6d3bf834ecbe37cadfd7e269745b8
Yusuke Koga <ykoga07@bu.edu>
2018-09-04 12:36:18 -0400

    Added grep capability to featureModuleLookup, fixed error in examples of plotDimReduceGene

--- R/plot_dr.R
+++ R/plot_dr.R
@@ -44,7 +44,7 @@ plotDimReduceGrid = function(dim1, dim2, matrix, size, xlab, ylab, color_low, co
 #' @examples
 #' celda.tsne <- celdaTsne(counts = celdaCG.sim$counts, celda.mod = model_CG)
 #' plotDimReduceGene(dim1 = celda.tsne[,1],dim2 = celda.tsne[,2],
-#'                   counts = celdaCG.sim$counts,features = c("Gene_99"), exact.match = TRUE))
+#'                   counts = celdaCG.sim$counts,features = c("Gene_99"), exact.match = TRUE)
 #' @export 
 plotDimReduceGene = function(dim1, dim2, counts, features, exact.match = TRUE, trim = c(-2,2), size = 1, xlab = "Dimension_1", ylab = "Dimension_2", color_low = "grey", color_mid = NULL, color_high = "blue"){
   counts = normalizeCounts(counts, transformation.fun = sqrt, scale.fun = base::scale)
