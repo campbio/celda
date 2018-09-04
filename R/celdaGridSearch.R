eb2b122db6f16f946177812a9894411afaa8b555
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-09-03 22:49:04 -0400

    Updated docs for celdaGridSearch, subsetCeldaList, and selectBestModel
    Former-commit-id: cd800ab7aedd02ad7dfaafcde95085d5908d7688

--- R/celdaGridSearch.R
+++ R/celdaGridSearch.R
@@ -9,40 +9,49 @@ available_models = c("celda_C", "celda_G", "celda_CG")
 #' 
 #' @param counts A count matrix.
 #' @param model Celda model. Options available in `celda::available.models`.
+#' @param params.test List. A list denoting the combinations of parameters to run in a celda model. For example, "list(K=5:10, L=15:20)" will run all combinations of K from 5 to 10 and L from 15 to 20 in model 'celda_CG'.
+#' @param params.fixed List. A list denoting additional parameters to use in each celda model. Default NULL.
 #' @param max.iter Integer. Maximum number of iterations of Gibbs sampling to perform. Default 200. 
 #' @param nchains Integer. Number of random cluster initializations. Default 1. 
-#' @param best.only Logical. Whether to return only the chain with the highest log likelihood per combination of parameters. Default TRUE. 
 #' @param cores Integer. The number of cores to use for parallel Gibbs sampling. Default 1.
+#' @param best.only Logical. Whether to return only the chain with the highest log likelihood per combination of parameters or return all chains. Default TRUE. 
 #' @param seed Integer. Passed to set.seed(). Default 12345.  
 #' @param verbose Logical. Whether to print log messages during celda chain execution. Default TRUE. 
 #' @param logfile.prefix Character. Prefix for log files from worker threads and main process. Default "Celda". 
 #' @return Object of class "celda_list", which contains results for all model parameter combinations and summaries of the run parameters
 #' @examples
-#' celda.sim = simulateCells(model="celda_CG")
-#' celda.mods = celdaGridSearch(celda.sim$counts, model="celda_CG", 
-#'                              sample.label=celda.sim$sample.label,
-#'                              K.to.test=2:4, L.to.test=9:11, max.iter=2, nchains=1)
+#' ## Simulate a small dataset with 5 cell clusters and 10 feature modules
+#' celda.sim = simulateCells(model="celda_CG", K=5, L=10)
+#'
+#' ## Run various combinations of parameters with 'celdaGridSearch'
+#' celda.gs = celdaGridSearch(celda.sim$counts, model="celda_CG", 
+#'                              params.test=list(K=4:6, L=9:11),
+#'								params.fixed=list(sample.label=celda.sim$sample.label))                              
 #' @import foreach
 #' @export
-celdaGridSearch = function(counts, model, params,
-				    max.iter=200, cores=1, best.only=TRUE,
-				    seed=12345, verbose=TRUE, nchains=3,
+celdaGridSearch = function(counts, model, params.test, params.fixed=NULL,
+				    max.iter=200, nchains=3, cores=1,
+				    best.only=TRUE, seed=12345, verbose=TRUE, 
                     logfile.prefix="Celda") {
  
   ## Check parameters
   model.params = as.list(formals(model))
-  if(!all(names(params) %in% names(model.params))) {
-    bad.params = setdiff(names(params), names(model.params))
-    stop(paste0("The following elements in 'params' are not arguments of '", model, "': ", paste(bad.params, collapse=",")))
+  if(!all(names(params.test) %in% names(model.params))) {
+    bad.params = setdiff(names(params.test), names(model.params))
+    stop(paste0("The following elements in 'params.test' are not arguments of '", model, "': ", paste(bad.params, collapse=",")))
+  }
+  if(!is.null(params.fixed) && !all(names(params.fixed) %in% names(model.params))) {
+    bad.params = setdiff(names(params.fixed), names(model.params))
+    stop(paste0("The following elements in 'params.fixed' are not arguments of '", model, "': ", paste(bad.params, collapse=",")))
   }
   
   model.params.required = setdiff(names(model.params[model.params == ""]), "counts")
-  if(!all(model.params.required %in% names(params))) {
-    missing.params = setdiff(model.params.required, names(params))
-    stop(paste0("The following arguments are not in 'params' but are required for '", model, "': ", paste(missing.params, collapse=",")))
+  if(!all(model.params.required %in% c(names(params.test), names(params.fixed)))) {
+    missing.params = setdiff(model.params.required, c(names(params.test), names(params.fixed)))
+    stop(paste0("The following arguments are not in 'params.test' or 'params.fixed' but are required for '", model, "': ", paste(missing.params, collapse=",")))
   }
-  if(any(c("z.init", "y.init") %in% names(params))) {
-    stop("Setting initialization parameters such as 'z.init' and 'y.init' in 'params' is not currently supported.")
+  if(any(c("z.init", "y.init", "sample.label") %in% names(params.test))) {
+    stop("Setting parameters such as 'z.init', 'y.init', and 'sample.label' in 'params' is not currently supported.")
   }
   
   # Set up parameter combinations for each individual chain
@@ -92,7 +101,7 @@ celdaGridSearch = function(counts, model, params,
   logliks = sapply(res.list, function(mod) { mod[["finalLogLik"]] })
   run.params = cbind(run.params, log_likelihood=logliks)
     
-  celda.res = list(run.params=run.params, params=params, res.list=res.list, 
+  celda.res = list(run.params=run.params, res.list=res.list, 
                    content.type=model, count.checksum=count.checksum)
   class(celda.res) = c("celda_list", model)
   
@@ -112,20 +121,22 @@ celdaGridSearch = function(counts, model, params,
 ################################################################################
 # Methods for manipulating celda_list objects                                  #
 ################################################################################
-#' Select models from a list of results generated in a celda run.
+#' Select a subset of models from a 'celda_list' object generated by celdaGridSearch.
 #' 
 #' Convenience function for picking out specific models from a celda_list object.
 #' Models can be selected by various parameters, most importantly the K/L parameters (number of cell
 #'  clusters / number of feature clusters). 
 #' 
 #' @param celda.list Object of class "celda_list". An object containing celda models returned from `celdaGridSearch()`.
-#' @return A celda model object matching the provided parameters, or a list of celda model objects if multiple models were matched (of class "celda_C", "celda_G", "celda_CG" accordingly), or NA if one is not found.
+#' @return A new 'celda_list' object containing all models matching the provided criteria in 'params'. If entry in the list matches, the results for the matching model will be returned.
 #' @examples
-#' celda.mods = celdaGridSearch(celda::pbmc_select, model="celda_CG", K.to.test=c(10,15), 
-#'                       L.to.test=c(50, 100), max.iter=2, nchains=1)
-#' desired.mod = filterCeldaList(celda.mods, K=10, L=100)
+#' celda.sim = simulateCells(model="celda_CG", K=5, L=10)
+#' celda.gs = celdaGridSearch(celda.sim$counts, model="celda_CG", 
+#'                              params.test=list(K=4:6, L=9:11),
+#'								params.fixed=list(sample.label=celda.sim$sample.label))
+#' res.K5.L10 = subsetCeldaList(celda.gs, params=list(K=5, L=10))
 #' @export
-filterCeldaList = function(celda.list, params) {
+subsetCeldaList = function(celda.list, params) {
   if (!isTRUE(class(celda.list)[1] == "celda_list")) stop("celda.list parameter was not of class celda_list.")
 
   ## Check for bad parameter names
@@ -156,7 +167,7 @@ filterCeldaList = function(celda.list, params) {
 }
 
 
-#' Select the best model from a celda_list by final log-likelihood.
+#' Select models with the best log likelihood from a 'celda_list' object gererated by celdaGridSearch.
 #' 
 #' This function returns the celda model from a celda_list object containing
 #' the maxiumim final log-likelihood. K, L, or combination of K and L must be 
@@ -166,9 +177,11 @@ filterCeldaList = function(celda.list, params) {
 #' @param celda.list Object of class "celda_list". An object containing celda models returned from `celdaGridSearch()`.
 #' @return The celda model object with the highest finalLogLik attribute, meeting any K/L criteria provided
 #' @examples
-#' celda.mods = celdaGridSearch(celda::pbmc_select, model="celda_CG", K.to.test=c(5,10), 
-#'                       L=c(10,20,30), max.iter=2, nchains=1)
-#' best.mod.k5.l20 = selectBestModel(celda.mods, K=5, L=20)
+#' ## Run various combinations of parameters with 'celdaGridSearch'
+#' celda.gs = celdaGridSearch(celda.sim$counts, model="celda_CG", params.test=list(K=4:6, L=9:11), params.fixed=list(sample.label=celda.sim$sample.label), best.only=FALSE)
+#'
+#' ## Returns same result as running celdaGridSearch with "best.only = TRUE"
+#' celda.gs.best = selectBestModel(celda.gs)  
 #' @export
 selectBestModel = function(celda.list) {
   if (!isTRUE(class(celda.list)[1] == "celda_list")) stop("celda.list parameter was not of class celda_list.")
