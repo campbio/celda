81f3bfde8ccefa9e18fac8c8d9664c72b77dd8ab
Yusuke Koga <ykoga07@bu.edu>
2018-09-06 18:54:22 -0400

    Removed line from test
    Former-commit-id: c1cc69883a6be7756c3f506a138b705107ead5e9

--- tests/testthat/test-celda_C.R
+++ tests/testthat/test-celda_C.R
@@ -92,7 +92,6 @@ test_that(desc = "Checking topRank to see if it runs without errors", {
   expect_equal(names(top.rank),
                c("index","names"))
   top.rank <- topRank(matrix = factorized$proportions$gene.states, n = 1000)
-  expect_equal(names(top.rank),
 })
 
 # plotHeatmap
