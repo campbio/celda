4df1d8e49c7ecf62446d614e658951349f2cb5fa
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-09-05 20:57:32 -0400

    Updated parameter name to 'model'

--- R/s3_generics.R
+++ R/s3_generics.R
@@ -77,16 +77,16 @@ celdaHeatmap <- function(counts, celda.mod, ...) {
 #' @examples
 #' celda.sim = simulateCells(model="celda_CG")
 #' celda.mod = "celda_CG"
-#' loglik = calculateLoglikFromVariables(celda.sim$counts, celda.mod, 
+#' loglik = calculateLoglikFromVariables(celda.sim$counts, model, 
 #'                                       sample.label=celda.sim$sample.label,
 #'                                       z=celda.sim$z, y=celda.sim$y,
 #'                                       K=celda.sim$K, L=celda.sim$L,
 #'                                       alpha=celda.sim$alpha, beta=celda.sim$beta,
 #'                                       gamma=celda.sim$gamma, delta=celda.sim$delta)
 #' @export
-calculateLoglikFromVariables = function(counts, celda.mod, ...) {
-  class(counts) = c(celda.mod)
-  do.call(paste("calculateLoglikFromVariables.", celda.mod, sep=""),
+calculateLoglikFromVariables = function(counts, model, ...) {
+  class(counts) = c(model)
+  do.call(paste("calculateLoglikFromVariables.", model, sep=""),
           list(counts, ...))
 }
 
