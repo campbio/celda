4a7a7627748af742f4edc3cd6a475e529a43ec71
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2018-09-05 21:39:19 -0400

    Updated test units for calculatePerplexityFromVariables

--- tests/testthat/test-celda_C.R
+++ tests/testthat/test-celda_C.R
@@ -49,7 +49,7 @@ test_that(desc = "Testing celdaGridSearch with celda_C", {
 
 # calculateLoglikFromVariables
 test_that(desc = "Testing calculateLoglikFromVariables.celda_C", {
-  expect_lt(calculateLoglikFromVariables(celda.mod="celda_C", 
+  expect_lt(calculateLoglikFromVariables(model="celda_C", 
                                          counts = celdaC.sim$counts,z = celdaC.sim$z,
                                          K = celdaC.sim$K, alpha = 1, beta = 1,
                                          sample.label = celdaC.sim$sample.label), 0)
