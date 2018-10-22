b1a99103aafde60c1e4873303c2060af7554a500
Yusuke Koga <ykoga07@bu.edu>
2018-10-22 11:18:59 -0400

    Edits to vignette

--- inst/doc/celda-analysis.R
+++ inst/doc/celda-analysis.R
@@ -1,28 +1,13 @@
 ## ----eval=FALSE------------------------------------------------------------
-#  library(devtools)
-#  source("https://bioconductor.org/biocLite.R")
-#  install.packages("Rcpp")
-#  biocLite("SummarizedExperiment")
-#  biocLite("MAST")
-#  install_github("compbiomed/celda")
-
-## ----eval=FALSE------------------------------------------------------------
 #  library(celda)
 
 ## ----eval=FALSE------------------------------------------------------------
 #  help(package=celda)
 
 ## ----eval=T, warning = FALSE, echo = FALSE, message = FALSE----------------
-library(devtools)
 library(celda)
-library(Matrix)
-library(gtools)
-library(ggplot2)
-library(Rtsne)
-library(reshape2)
 
 ## --------------------------------------------------------------------------
-getwd()
 sim_counts <- simulateCells("celda_CG", K = 5, L = 10, S = 5, G = 500)
 
 ## --------------------------------------------------------------------------
@@ -45,34 +30,30 @@ table(celda.model$z, sim_counts$z)
 table(celda.model$y, sim_counts$y)
 
 
-## ---- eval=FALSE, fig.width = 8, fig.height = 8, warning = FALSE, message = FALSE----
-#  celdaHeatmap(counts = sim_counts$counts, celda.mod = celda.model)
-
 ## --------------------------------------------------------------------------
 factorized <- factorizeMatrix(counts = sim_counts$counts, celda.mod = celda.model)
 names(factorized)
 
 ## --------------------------------------------------------------------------
-dim(factorized$proportions$module)
-head(factorized$proportions$module)
-
-## --------------------------------------------------------------------------
 dim(factorized$proportions$cell)
-factorized$proportions$cell[,1:3]
+head(factorized$proportions$cell[,1:3], 5)
 
 ## --------------------------------------------------------------------------
 cell.pop <- factorized$proportions$cell.population
 dim(cell.pop)
-cell.pop
+head(cell.pop, 5)
 
 ## --------------------------------------------------------------------------
-top.genes <- topRank(matrix = factorized$proportions$module, n = 50, threshold = NULL)
+dim(factorized$proportions$module)
+head(factorized$proportions$module, 5)
+
+## --------------------------------------------------------------------------
+top.genes <- topRank(matrix = factorized$proportions$module, n = 10, threshold = NULL)
 
 ## --------------------------------------------------------------------------
 top.genes$names$L1
 
 ## ---- eval = FALSE, fig.width = 8, fig.height = 8--------------------------
-#  top.genes.ix <- unique(unlist(top.genes$index))
 #  celdaHeatmap(counts = sim_counts$counts, celda.mod = celda.model, nfeatures = 10)
 
 ## --------------------------------------------------------------------------
@@ -93,10 +74,10 @@ tsne <- celdaTsne(counts = sim_counts$counts, celda.mod = celda.model)
 #  celdaProbabilityMap(counts = sim_counts$counts, celda.mod = celda.model)
 
 ## ---- eval = FALSE, fig.width = 8, fig.height = 8--------------------------
-#  moduleHeatmap(counts = sim_counts$counts, celda.mod = celda.model, feature.module = 1:2)
+#  moduleHeatmap(counts = sim_counts$counts, celda.mod = celda.model, feature.module = 1, top.cells = 100)
 
 ## --------------------------------------------------------------------------
-genes = c("Gene_1","Gene_2","Gene_3")
+genes = c(top.genes$names$L1, top.genes$names$L10)
 gene.ix = which(rownames(sim_counts$counts) %in% genes)
 norm.counts <- normalizeCounts(counts = sim_counts$counts, scale.fun = scale)
 
@@ -104,30 +85,26 @@ norm.counts <- normalizeCounts(counts = sim_counts$counts, scale.fun = scale)
 #  plotHeatmap(counts = norm.counts, z = celda.model$z, y = celda.model$y, feature.ix = gene.ix, show.names.feature = TRUE)
 
 ## ----message=FALSE---------------------------------------------------------
-diff.exp.clust1 <- differentialExpression(counts = sim_counts$counts, 
+diffexp.clust1 <- differentialExpression(counts = sim_counts$counts, 
                                             celda.mod = celda.model, 
                                             c1 = 1, c2 = NULL)
 
-head(diff.exp.clust1,10)
+head(diffexp.clust1,5)
 
 ## ---- message=FALSE--------------------------------------------------------
-diff.exp.clust1vs2 <- differentialExpression(counts = sim_counts$counts, celda.mod = celda.model, c1 = 1, c2 = 2)
+diffexp.clust1vs2 <- differentialExpression(counts = sim_counts$counts, celda.mod = celda.model, c1 = 1, c2 = 2)
 
-diff.exp.clust1vs2 <- diff.exp.clust1vs2[diff.exp.clust1vs2$fdr < 0.25,]
+diffexp.clust1vs2 <- diffexp.clust1vs2[diffexp.clust1vs2$FDR < 0.05 & abs(diffexp.clust1vs2$Log2_FC) > 2,]
+
+head(diffexp.clust1vs2, 5)
 
 ## --------------------------------------------------------------------------
-upreg.genes <- head(diff.exp.clust1vs2[order(diff.exp.clust1vs2$log2fc, decreasing = TRUE),],10)
-
-## --------------------------------------------------------------------------
-upreg.gene.ix = which(rownames(sim_counts$counts) %in% upreg.genes$Gene)
+diffexp.gene.ix = which(rownames(sim_counts$counts) %in% diffexp.clust1vs2$Gene)
 
 norm.counts <- normalizeCounts(counts = sim_counts$counts, scale.fun = scale)
 
 ## ---- eval = FALSE---------------------------------------------------------
-#  plotHeatmap(counts = norm.counts, z = celda.model$z, y = celda.model$y, feature.ix = upreg.gene.ix, show.names.feature = TRUE)
-
-## ---- eval = FALSE, fig.height = 8, fig.width = 8--------------------------
-#  plotDimReduceFeature(dim1 = tsne[,1], dim2 = tsne[,2], counts = sim_counts$counts, features = upreg.genes$Gene[1:9])
+#  plotHeatmap(counts = norm.counts[,celda.model$z %in% c(1,2)], z = celda.model$z[celda.model$z %in% c(1,2)], y = celda.model$y, feature.ix = diffexp.gene.ix, show.names.feature = TRUE)
 
 ## ---- eval = FALSE, message = FALSE----------------------------------------
 #  cgs <- celdaGridSearch(sim_counts$counts,
@@ -162,26 +139,17 @@ norm.counts <- normalizeCounts(counts = sim_counts$counts, scale.fun = scale)
 #                                       celda.list = cgs,
 #                                       resample = 2)
 #  
-#  celda.model.list = subsetCeldaList(celda.list = cgs, params = list(K = 5, L = 10))
+#  cgs.K5.L10 = subsetCeldaList(celda.list = cgs, params = list(K = 5, L = 10))
 #  
-#  celda.model1 <- selectBestModel(celda.list = celda.model.list)
-#  
-#  celda.model2 <- subsetCeldaList(celda.list = cgs, params = list(K = 5, L = 10, chain = 2))
+#  celda.model1 <- selectBestModel(celda.list = cgs.K5.L10)
 
 ## --------------------------------------------------------------------------
 featureModuleLookup(counts = sim_counts$counts, celda.mod = celda.model, 
                           feature = c("Gene_99"))
 
 ## --------------------------------------------------------------------------
-celda.model.y.recoded <- recodeClusterY(celda.mod = celda.model, from = c(1,2,3,4,5), to = c(2,1,3,4,5))
-
 celda.model.z.recoded <- recodeClusterZ(celda.mod = celda.model, from = c(1,2,3,4,5), to = c(2,1,3,4,5))
 
 ## --------------------------------------------------------------------------
-table(celda.model$z)
-table(celda.model$y)
-
-## --------------------------------------------------------------------------
-table(celda.model.z.recoded$z)
-table(celda.model.y.recoded$y)
+table(celda.model$z, celda.model.z.recoded$z)
 
