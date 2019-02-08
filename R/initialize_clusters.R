58afbabc87465131981333608c8e6fa981bd9aff
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2019-02-07 19:05:05 -0500

    Initital cluster split

--- R/initialize_clusters.R
+++ R/initialize_clusters.R
@@ -50,42 +50,68 @@ initialize.cluster = function(N, len, z = NULL, initial = NULL, fixed = NULL, se
 }
 
 
-recursive.splitZ = function(counts, s, K, alpha, beta, min.cell = 3, seed=12345) {
+recursive.splitZ = function(counts, s, K, K.subcluster, split.all=TRUE, alpha, beta, min.cell = 3, seed=12345) {
   current.K = 2
   overall.z = rep(1, ncol(counts))
   cluster.splits = matrix(NA, nrow=ncol(counts), ncol=K)
   cluster.split.flag = rep(TRUE, K)
-    
+  
+  res = .celda_C(counts, K=K.subcluster, max.iter=20, split.on.iter=-1, split.on.last=FALSE, verbose=FALSE)
+  overall.z = as.integer(as.factor(res@clusters$z))
+  current.K = length(unique(overall.z)) + 1
+  
+  clust.split = vector("list", K)    
   while(current.K <= K) {
-
-    z.ta = tabulate(overall.z, K)
+    z.ta = tabulate(overall.z, max(overall.z))
     z.to.split = which(z.ta > min.cell)
 
-    ## Split each cluster <= current.K and number of cells > min.cell
     for(i in z.to.split) {
-      if(isTRUE(cluster.split.flag[i])) {
-        ix = which(overall.z == i)
-        res = suppressMessages(.celda_C(counts[,ix], K=2, stop.iter = 1, split.on.iter=-1, split.on.last=FALSE, nchains=1, seed=seed, verbose=FALSE, initialize="random"))
-        cluster.splits[cbind(ix, i)] = res@clusters$z
-        cluster.split.flag[i] = FALSE
-      }
+      if(is.null(clust.split[[i]]) || is.na(clust.split[[i]])) {
+        if(z.ta[i] <= K.subcluster) {
+          clust.split[[i]] = seq.int(1, z.ta[i]) 
+        } else {
+          clustLabel = .celda_C(counts[,overall.z == i,drop=FALSE], K=K.subcluster, max.iter=20, split.on.iter=-1, split.on.last=FALSE, verbose=FALSE)
+          clust.split[[i]] = as.integer(as.factor(clustLabel@clusters$z))
+        }  
+      } 
     }
 
-    ## Calculate likelihood for each cluster split
-    temp.z = matrix(overall.z, nrow=ncol(counts), ncol=length(z.to.split))
-  	ll = vector(mode="numeric", length=ncol(temp.z))    
-  	for(i in 1:ncol(temp.z)) {
-  	  temp.z[cluster.splits[,i] == 2,i] = current.K 
-  	  ll[i] = logLikelihood.celda_C(counts, "celda_C", s, temp.z[,i], current.K, alpha, beta)	  
-  	}  
+  	if(isTRUE(split.all)) {
+  	  for(i in z.to.split) {
+  	    split.ix = clust.split[[i]] > 1
+  	    ix = overall.z == i
+  	    new.z = overall.z[ix]
+  	    
+  	    new.z[split.ix] = current.K + clust.split[[i]][split.ix] - 2
+  	    
+  	    overall.z[ix] = new.z
+  	    current.K = max(overall.z) + 1
+  	    clust.split[[i]] = NA
+  	    if(current.K > K) {
+  	      break()
+  	    }
+  	  }   
+  	} else {
+  	  ## Calculate likelihood for each cluster split
+  	  temp.z = matrix(overall.z, nrow=ncol(counts), ncol=length(z.to.split))
+  	  ll = vector(mode="numeric", length=ncol(temp.z))    
+  	  for(i in 1:ncol(temp.z)) {
+  	    split.ix = clust.split[[z.to.split[i]]] > 1
+  	    ix = overall.z == z.to.split[i]
+  	    new.z = temp.z[ix,i]
+  	    new.z[split.ix] = current.K + clust.split[[z.to.split[i]]][split.ix] - 2
+  	    temp.z[ix,i] = new.z
+  	    temp.K = max(temp.z[,i])
+    	  ll[i] = logLikelihood.celda_C(counts, "celda_C", s, temp.z[,i], temp.K, alpha, beta)	    	  
 
-    ## Choose best split. Reset flags so the old cluster will be re-evaluated for splitting
-    best.ix = which.max(ll)
-  	overall.z = temp.z[,best.ix]  
-  	cluster.splits[,z.to.split[best.ix]] = NA
-    cluster.split.flag[z.to.split[best.ix]] = TRUE
-    	
-	  current.K = current.K + 1
+    	  ## Choose best split. Reset flags so the old cluster will be re-evaluated for splitting
+    	  best.ix = which.max(ll)
+    	  overall.z = temp.z[,best.ix]  
+    	  clust.split[[z.to.split[best.ix]]] = NA
+    	  
+    	  current.K = max(overall.z, na.rm=TRUE) + 1  
+    	}
+  	}
   }
   
   return(overall.z)
@@ -170,3 +196,94 @@ recursive.splitY = function(counts, L, beta, delta, gamma, z=NULL, K=NULL, K.sub
   
   return(overall.y)
 } 
+
+
+
+recursive.splitY_v2 = function(counts, L, temp.cell.cluster=1000, cell.cluster.breaks=10, beta=1, delta=1, gamma=1, min.feature=3, seed=12345) {
+  
+  s = rep(1, ncol(counts))
+  if(!is.null(temp.cell.cluster) && temp.cell.cluster > 0) {
+    new.z = recursive.splitZ(counts, s, K=temp.cell.cluster, K.subcluster=cell.cluster.breaks, split.all=TRUE, alpha=1, beta=1, min.cell = 3, seed=seed)
+    counts = colSumByGroup(counts, new.z, length(unique(new.z)))
+  }
+  
+  model.initial = .celda_G(counts, L=2, split.on.iter=-1, split.on.last=FALSE, nchains=1, verbose=T)
+
+  ## Perform splitting for y labels
+  current.L = 3
+  overall.y = model.initial@clusters$y
+  
+  y.history = matrix(NA, nrow=nrow(counts), ncol=L)
+  y.history[,2] = overall.y
+  
+  ll.history = c()
+  
+
+  while(current.L <= L) {
+print(paste0("currentL: ", current.L))
+    y.ta = tabulate(overall.y, L)
+    y.to.split = which(y.ta > min.feature)
+
+    clust.split = vector("list", current.L)
+    for(i in y.to.split) {
+      if(is.null(clust.split[[i]]) || is.na(clust.split[[i]])) {
+        if(y.ta[i] >= min.feature) {
+          print(i)
+          clustLabel = .celda_G(counts[overall.y == i,,drop=FALSE], L=2, split.on.iter=-1, split.on.last=FALSE, nchains=1, verbose=FALSE)
+          clust.split[[i]] = as.integer(as.factor(clustLabel@clusters$y))
+          if(length(unique(clust.split[[i]])) != 2) {
+            y.to.split = setdiff(y.to.split, i)
+          }
+        }  
+      } 
+    }
+    
+
+    ## Calculate likelihood for each cluster split
+    temp.y = matrix(overall.y, nrow=nrow(counts), ncol=length(y.to.split))
+    ll = vector(mode="numeric", length=ncol(temp.y))    
+    for(i in 1:ncol(temp.y)) {
+      ix = overall.y == y.to.split[i]
+      temp.y[ix,i] = ifelse(clust.split[[y.to.split[i]]] == 2, y.to.split[i], current.L)
+      ll[i] = logLikelihood.celda_G(counts=counts, y=temp.y[,i], L=current.L, beta=beta, delta=delta, gamma=gamma)
+    }  
+    
+    ## Choose best split. Reset flags so the old cluster will be re-evaluated for splitting
+    best.ix = which.max(ll)
+    #temp.y = temp.y[,best.ix] 
+    temp.model = .celda_G(counts, L=current.L, stop.iter=5, split.on.iter=-1, split.on.last=FALSE, nchains=1, verbose=FALSE, y.init=temp.y[,best.ix])
+    overall.y = temp.model@clusters$y
+    
+    ## Get list of modules whose features have changed membership.
+    ##The best split for these clusters need to b
+    prev.y = y.history[,current.L-1]
+    module.change = unique(prev.y[overall.y != prev.y])
+    print(paste0("module change: ", module.change))
+    for(i in module.change) {
+      clust.split[[i]] = NA 
+    }
+    ll.history = c(ll.history, temp.model@finalLogLik)
+    clust.split = vector("list", L)
+    y.history[,current.L] = overall.y
+    current.L = current.L + 1
+  }
+  
+  return(list(y=y.history, ll=ll.history))
+} 
+
+splitModule = function(counts, celda.mod, module, n=2) {
+  ix = celda.mod@clusters$y == module
+  new.L = max(celda.mod@clusters$y) + n - 1
+  if(sum(ix) > 1) {
+    temp.model = .celda_G(counts[ix,,drop=FALSE], L=n, split.on.iter=-1, split.on.last=FALSE, nchains=1, verbose=FALSE)  
+    new.y = celda.mod@clusters$y
+    new.y[ix] = ifelse(temp.model@clusters$y == 1, module, new.L)
+    model = .celda_G(counts, L=new.L, split.on.iter=-1, split.on.last=FALSE, nchains=1, verbose=FALSE, y.init=new.y)
+  } else {
+    model = celda.mod
+  }
+  
+  return(model)
+}
+
+
