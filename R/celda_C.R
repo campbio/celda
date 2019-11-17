04939adaae3fc41b3691d1e8d9a3d4661180b48c
Irisapo <syyang@bu.edu>
2019-11-17 00:34:00 -0500

    index bug

--- R/celda_C.R
+++ R/celda_C.R
@@ -458,25 +458,25 @@ celda_C <- function(counts,
 		## when consider a specific cluster fo this cell, no need to calculate cells in other cluster	
         for (j in seq_len(K)) {
             #otherIx <- seq(K)[-j]
-				if (j != z[i]) { # when j is not current population assignment
-						probs[j, i] <- log(mCPByS[j, s[i]] + alpha) + ## Theta simplified
-						sum(lgamma(nGByCP[j] + counts[, i] + beta)) -   # if adding this cell -- Phi Numerator
-						lgamma(nCP[j] + nByC[i] + nG * beta) -    # if adding this cell -- Phi Denominator
-						sum(lgamma(nGByCP[j] + beta)) +   # if without this cell -- Phi Numerator
-						lgamma(nCP[j] + nG * beta  )    # if without this cell -- Phi Denominator
-						#sum(nGByCP1[otherIx]) + ## Phi Numerator (other cells)
-						#nGByCP2[j] - ## Phi Numerator (current cell)
-						#sum(nCP1[otherIx]) - ## Phi Denominator (other cells)
-						#nCP2[j] - ## Phi Denominator (current cell)
-						}
-				else {  # when j is current population assignment 
-						probs[j, i] <- log(mCPByS[j, s[i]] + alpha) + ## Theta simplified
-						sum(lgamma(nGByCP[j] + beta)) - 
-						lgamma(nCP[j] + nG * beta) -
-						sum(lgamma(nGByCP[j] - counts[, i] + beta)) +
-						lgamma(nCP[j] - nByC[i] + nG * beta) 
+		if (j != z[i]) { # when j is not current population assignment
+				probs[j, i] <- log(mCPByS[j, s[i]] + alpha) + ## Theta simplified
+				sum(lgamma(nGByCP[, j] + counts[, i] + beta)) -   # if adding this cell -- Phi Numerator
+				lgamma(nCP[j] + nByC[i] + nG * beta) -    # if adding this cell -- Phi Denominator
+				sum(lgamma(nGByCP[, j] + beta)) +   # if without this cell -- Phi Numerator
+				lgamma(nCP[j] + nG * beta  )    # if without this cell -- Phi Denominator
+				#sum(nGByCP1[otherIx]) + ## Phi Numerator (other cells)
+				#nGByCP2[j] - ## Phi Numerator (current cell)
+				#sum(nCP1[otherIx]) - ## Phi Denominator (other cells)
+				#nCP2[j] - ## Phi Denominator (current cell)
+		}
+		else {  # when j is current population assignment 
+				probs[j, i] <- log(mCPByS[j, s[i]] + alpha) + ## Theta simplified
+				sum(lgamma(nGByCP[, j] + beta)) - 
+				lgamma(nCP[j] + nG * beta) -
+				sum(lgamma(nGByCP[, j] - counts[, i] + beta)) +
+				lgamma(nCP[j] - nByC[i] + nG * beta) 
 
-						}
+		}
 
         }
 
