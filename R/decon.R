12e008ea54d8f4c7855217c2d7fad6e5f2c16c78
Irisapo <syyang@bu.edu>
2019-12-28 08:50:59 -0500

    fix mistake in mcmc fix-iteration

--- R/decon.R
+++ R/decon.R
@@ -182,16 +182,23 @@ simulateContaminatedMatrix <- function(C = 300,
 
     Pr <- exp(logPr) / (exp(logPr) + exp(logPc))
     Pc <- 1 - Pr
-    deltaV2 <-
-        MCMCprecision::fit_dirichlet(matrix(c(Pr, Pc), ncol = 2))$alpha
+    #deltaV2 <-
+    #    MCMCprecision::fit_dirichlet(matrix(c(Pr, Pc), ncol = 2))$alpha
 
     estRmat <- t(Pr) * counts
     rnGByK <- .colSumByGroupNumeric(estRmat, z, K)
     cnGByK <- rowSums(rnGByK) - rnGByK
 
+    TNbyC <- colSums(counts) 
+    estRbyCol <- colSums(estRmat)
+    
+    PrbyC <- estRbyCol / TNbyC
+    PcbyC <- 1 - PrbyC
+    deltaV2 <- MCMCprecision::fit_dirichlet(cbind(PrbyC, PcbyC))$alpha
+
     ## Update parameters
     theta <-
-        (colSums(estRmat) + deltaV2[1]) / (colSums(counts) + sum(deltaV2))
+        (estRbyCol + deltaV2[1]) / (TNbyC + sum(deltaV2))
     phi <- normalizeCounts(rnGByK,
         normalize = "proportion",
         pseudocountNormalize = 1e-20)
