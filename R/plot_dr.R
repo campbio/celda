bc3a8c0d0d53770224dc8d86a433ce236cc86aa5
zhewa <zhe@bu.edu>
2020-05-24 19:52:26 +0800

    fix example

--- R/plot_dr.R
+++ R/plot_dr.R
@@ -630,7 +630,7 @@ setMethod("plotDimReduceModule",
         colorHigh = "firebrick1",
         ncol = NULL) {
 
-        factorized <- factorizeMatrix(celdaMod = celdaMod, counts = x)
+        factorized <- factorizeMatrix(x = celdaMod, counts = x)
         g <- .plotDimReduceModule(dim1 = dim1,
             dim2 = dim2,
             counts = x,
