b6ef607dca4223c267e7c555fa264fbd72ee1493
Yuan <76789185+yuan-yin-truly@users.noreply.github.com>
2021-10-01 00:19:10 -0400

    Speed up sparse matrix assignmnet

--- R/decon.R
+++ R/decon.R
@@ -495,7 +495,22 @@ setMethod(
       z = as.integer(res$z),
       pseudocount = 1e-20
     )
-    estRmat[seq(nrow(counts)), which(batch == bat)] <- estRmat.temp
+    
+    # Speed up sparse matrix value assignment by cbind -> order recovery
+    
+    allCol <- paste0("col_", 1:ncol(estRmat))
+    colnames(estRmat) <- allCol
+    
+    subCol <- paste0("col_", which(batch == bat))
+    colnames(estRmat.temp) <- subCol
+    
+    estRmat <- estRmat[, !(allCol %in% subCol)]
+    estRmat <- cbind(estRmat, estRmat.temp)
+    
+    # Recover order
+    estRmat <- estRmat[, allCol]
+    
+    #(Old method) estRmat[seq(nrow(counts)), which(batch == bat)] <- estRmat.temp
     dimnames(estRmat) <- list(geneNames, allCellNames)
 
     resBatch[[bat]] <- list(
